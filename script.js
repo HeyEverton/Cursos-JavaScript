5b9071d8d62fc08e56ed447be8c8f402bac5fa81
Everton <everton.h.g.fonseca@gmail.com>
2022-06-29 16:00:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,88 @@
+//alert("Muito bom dia, vamos acordar!");//
+
+
+
+//document.getElementById("titulo").innerHTML = "Opa meu querido, quer café?"; //troca o texto do h1 com o id titulo, para esse texto dentro das aspas//
+//document.write("Texto exemplo");
+//window.alert("Mensagem exemplo");
+
+//console.log("Mensagem que vai aparecer no console");//
+
+//var nome = "Everton";
+//var idade = 90;
+//alert( nome);//
+
+//var x = 10;
+//var y = 15;
+
+//var total = x + y;//
+//alert(total);//
+
+//var nome = "Everton"; //TEXTO ENTRE ASPAS É CHAMADO DE STRING//
+//var sobrenome = "Henrique";
+
+//var nomecompleto = nome+" "+sobrenome;
+//alert(nomecompleto);//
+
+
+//var hora = 15;
+//if ( hora < 12 ) {
+       //console.log("Bom dia")
+//}
+//else {
+    //console.log("Deixa de ser burro, não é de manhã")
+//}
+//var hora = 15
+
+//if (hora >= 8 && hora <= 12) {
+//alert('Hora de programar!!!');
+//} else {
+//console.log('Hora de Jogar.');
+//}
+
+//var hora = 6;
+
+//if ( hora < 5) {//
+    //console.log("Boa madrugada")
+//}//else if ( hora < 12) {
+    //console.log("Bom dia");
+
+//} //else if ( hora < 18) {
+    //console.log("Boa tarde");
+//} //else if ( hora <= 23) {
+    //console.log("Boa noite");
+//}
+
+//var hora = 15;
+
+//if ( hora == 12 || hora == 18) {
+    //console.log("Você está na hora do rush");
+//}
+
+//var idade = 19;
+
+//if(idade < 18) { 
+//console.log("adolescente");
+//} else if (idade >=19 && idade <=50) {
+//console.log("adulto");
+//} else if(idade > 50) {
+//console.log("idoso");
+//} else {
+//console.log("operação inválida");
+//}
+
+
+//function clicou(){
+   //document.getElementById("titulo").innerHTML = "Agora beba. imediatamente."
+//}
+
+function digitou(e) {
+   if(e.keyCode == 13) {
+    
+   }
+}
+
+
+
+
+
