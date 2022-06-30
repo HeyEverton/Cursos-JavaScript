9ced450a675b1f1722b6464b6245bcdccf2b4ea5
Everton <everton.h.g.fonseca@gmail.com>
2022-06-30 16:02:14 -0300

    MOD 3 #6

---
+++ MOD - 3/assets/script.js
@@ -0,0 +1,26 @@
+//document.getElementById('exemplo').innerHTML = "Martins "; //PELO ID
+//document.getElementsByClassName('lista')[1].innerHTML = 'foi alterado'; //PELA CLASSE(S)
+//document.getElementsByTagName('button');   //PELA(S) TAG(S)
+//document.getElementsByName('email') //PELO NOME (MAIS EM CAMPO DE TEXTOS)
+
+
+//function verde() {
+//    document.querySelector("#exemplo").classList.add('verde');
+//    document.querySelector("#exemplo").classList.remove('vermelho');
+//    document.querySelector("#exemplo").classList.remove('azul');    
+//}
+//function vermelho() {
+//    document.querySelector("#exemplo").classList.remove('azul');  
+//    document.querySelector("#exemplo").classList.remove('verde');
+//    document.querySelector('#exemplo').classList.add('vermelho');
+//}
+//function azul() {
+//    document.querySelector("#exemplo").classList.remove('verde');
+//    document.querySelector('#exemplo').classList.remove('vermelho');
+//    document.querySelector('#exemplo').classList.add('azul');
+//}
+
+
+function trocar() {
+    document.querySelector
+}
