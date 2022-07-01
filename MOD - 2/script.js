d40014442fefc48ce63f4f45c98887726169f794
Everton <everton.h.g.fonseca@gmail.com>
2022-07-01 15:07:11 -0300

    MOD - 3 finalizado

--- MOD - 2/script.js
+++ MOD - 2/script.js
@@ -239,6 +239,23 @@
 //=============================FIM DO MÓDULO 2=========================================//
 
 
+//============================MÓDULO 3=================================================//
+
+
+
+
+//function trocar() {
+//
+//    if (document.querySelector('button').classList.contains('preto'))  {
+//        document.querySelector('button').classList.remove('preto');
+//        document.querySelector('button').classList.add('verde');
+//    } else {
+//        document.querySelector('button').classList.remove('verde');
+//        document.querySelector('button').classList.add('preto')
+//    }
+//}
+
+ 
 
 
 
--- MOD - 3/assets/script.js
+++ MOD - 3/assets/script.js
@@ -21,6 +21,34 @@
 //}
 
 
-function trocar() {
-    document.querySelector
-}
+//function trocarImagem(filename, animalname) {
+//    document.querySelector('.imagem').setAttribute('src', 'assets/img/'+filename);
+//    document.querySelector('.imagem').setAttribute('data-animal', animalname)
+//
+//}
+//function pegarAnimal() {
+//    let animal = document.querySelector('.imagem').getAttribute('data-animal');
+//    alert("O animal é "+animal);
+//}
+
+//function subirTela() {
+//    window.scrollTo({
+//        top: 0,
+//        left: 0,
+//        behavior: 'smooth'
+//        
+//    });
+//}
+//
+//function aparecerBotao() {
+//     if(window.scrollY === 0) {
+//        document.querySelector('.scrollbutton').style.display = 'none';
+//     } else {
+//        document.querySelector('.scrollbutton').style.display = 'block';
+//     }
+//}
+//
+//window.addEventListener('scroll', aparecerBotao);
+
+
+/*===========================FIM DO MÓDULO 3===============================*/
