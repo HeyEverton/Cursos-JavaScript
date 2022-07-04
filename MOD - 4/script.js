25331fbb5b965d7cb58772c3485e92a0247c4d58
Everton <everton.h.g.fonseca@gmail.com>
2022-07-04 13:49:18 -0300

    #18 mod-4

--- MOD - 4/script.js
+++ MOD - 4/script.js
@@ -39,22 +39,99 @@ for(let i in lista) {
 //        return false
 //    }
 //});
+//let lista = [
+//    {id:1, nome:'Everton', sobrenome:'Henrique'},
+//    {id:2, nome:'Lucas', sobrenome:'Medeiros'},
+//    {id:3, nome:'Armageddon', sobrenome:'Silva'}
+//];
+//let pessoa = lista.find(function(item){
+//    return  (item.id == 3) ? true : false; 
+//});
+//let res = pessoa;
+//console.log(res);
+//let d = new Date();
+//
+//d.setFullYear(2022);
+//
+//let novoValor = d.getTime();
+//console.log(novoValor);
+//let novoValor = Math.floor(Math.random() * 100);
+//
+//console.log(novoValor );
+
+//let  timer;
+//
+//function comecar() {
+//    timer = setInterval(showTime, 1000);
+//
+//}
+//function parar() {
+//    clearInterval(timer)
+//}
+//
+//function showTime() {
+//    let d = new Date();
+//    let h = d.getHours();
+//    let m = d.getMinutes();
+//    let s = d.getSeconds();
+//    let txt = h+':'+m+':'+s;
+//    document.querySelector('.demo').innerHTML = txt;
+//}
+//function rodar() {
+//  timer =  setTimeout(function() {
+//        document.querySelector('.demo').innerHTML = 'Rodou';
+//    }, 2000);
+//
+//}
+//function parar() {
+//    clearTimeout(timer);
+//}
+
+//let nome = 'Everton';
+//let idade = 36;
 
 
-let lista = [
-    {id:1, nome:'Everton', sobrenome:'Henrique'},
-    {id:2, nome:'Lucas', sobrenome:'Medeiros'},
-    {id:3, nome:'Armageddon', sobrenome:'Silva'}
-];
-let pessoa = lista.find(function(item){
-    return  (item.id == 3) ? true : false; 
-});
+//let frase = 'meu nome é '+nome+', eu tenho '+idade+' anos e ano que vem farei '+(idade+1)+' anos'
+//let frase = `Meu nome é ${nome}, eu tenho ${idade} e ano que vem farei ${idade+1} anos.`;
+
+//console.log(frase);
+//et pessoa = {
+//   nome: 'Everton',
+//   sobrenome: 'Henrique',
+//   idade: 90,
+//   social: {
+//       facebook: 'Kozuki Oden',
+//       instagram: {
+//           url: '@kozuki_',
+//           seguidores: '1000'
+//
+//       }
+//   }, 
+    //nomeCompleto: function() {
+        //return `${this.nome} ${this.sobrenome}`;
+    //}
+//}
+//let {nome, idade, social:{instagram} } = pessoa;
+//console.log(nome, idade,  instagram)
+//function pegarNomeCompleto (obj) {
+//    return obj.nome+' '+obj.sobrenome
+//}
+//
+//
+//console.log(pegarNomeCompleto(pessoa));
 
 
+//let info = [ 'Everton Henrique', 'Everton', 'Henrique', '@e.henrique9' ];
+//
+//
+//let [ nomeComleto, nome, sobrenome, instagram ] = info;
+//
+//console.log(nomeComleto, nome, sobrenome, instagram);
 
-let res = pessoa;
 
-console.log(res);
+let [nome, sobrenome] = ['Everton', 'Henrique'];
+
+console.log(nome, sobrenome);
 
 
 
